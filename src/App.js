9f59caebbccfa2086dcd13b528a64a3936731f59
Tra-127 <84733477+Tra-127@users.noreply.github.com>
2021-11-15 17:13:36 -0800

    add new todo list function, dectructure props

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import AddTodoForm from "./AddTodoForm";
 
 function App() {
 
-    const [newTodo, setNewTodo] = React.useState('');
+    //const [newTodo, setNewTodo] = React.useState();
+    const [todoList, setTodoList] = React.useState([]);
 
+    const  addTodo = (newTodo) => {
+        setTodoList([...todoList, newTodo]);
+    }
 
     return (
         <div>
             <h1> Todo List </h1>
-            <AddTodoForm onAddTodo={setNewTodo}></AddTodoForm>
-            <p> {newTodo} </p>
-            <TodoList></TodoList>
+            <AddTodoForm onAddTodo={addTodo}/>
+            <TodoList todoList={todoList}/>
         </div>
     );
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,16 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-const todoList = [
-    {
-        id: 0,
-        title: "Make an appointment",
-    },
-    {
-        id: 1,
-        title: "Finish HW1",
-    },
-    {
-        id:2,
-        title: "Return books"
-    }
-];
 
-
-const TodoList = () => {
+const TodoList = ({todoList}) => {
     return(
         <ul>
             {todoList.map(function(item){
                 return(
-                    <TodoListItem key={item.id} item={item}></TodoListItem>
+                    <TodoListItem todo={item} key={item.id}></TodoListItem>
                 );
             })}
+
         </ul>
     );
 };
